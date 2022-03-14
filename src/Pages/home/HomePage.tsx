d4b9003693338aebdc4e311651fec55cfbbcdc3d
Masud Afsar <masud@afsar.info>
2022-03-15 00:56:10 +0330

    Implement tasks list

--- src/Pages/home/HomePage.tsx
+++ src/Pages/home/HomePage.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import {Container} from '../../components/container/Container';
 import {AddTaskForm} from "../../components/form/AddTaskForm";
 
 import styles from './HomePage.module.scss';
+import {TasksContext} from "../../contexts/TasksContext";
+import {TaskCard} from "../../components/task/TaskCard";
 
 export const HomePage: React.FC = () => {
+    const {getTasks} = useContext(TasksContext);
+    const tasks = getTasks();
     return (
         <div className={styles.HomePage}>
             <Container>
@@ -15,7 +19,9 @@ export const HomePage: React.FC = () => {
                 <Container>
                     <h2 className={styles.Title}>Tasks</h2>
                 </Container>
-                <div className={styles.Tasks}></div>
+                <div className={styles.Tasks}>
+                    {tasks?.map(task => (<TaskCard key={task.id} task={task}/>))}
+                </div>
             </div>
         </div>
     );
---
+++ src/components/task/TaskCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {Icon} from "@iconify/react";
+import EditIcon from "@iconify/icons-ic/twotone-edit";
+
+import {Task} from "../../types/task";
+
+import styles from './TaskCard.module.scss';
+
+interface Props {
+    task: Task
+}
+
+export const TaskCard: React.FC<Props> = ({task}) => {
+    return (
+        <div className={styles.TaskCard}>
+            <h3 className={styles.Title}>{task.title}</h3>
+            <p className={styles.Description}>{task.description}</p>
+            <div className={styles.Actions}>
+                <span className={styles.Status}>{task.status}</span>
+                <Link to={'/ss'}>
+                    <Icon icon={EditIcon}/>
+                </Link>
+            </div>
+        </div>
+    );
+}
