99b3aa80340f97f11518331d2f3cfe9eaed29a2d
Masud Afsar <masud@afsar.info>
2022-03-14 13:23:33 +0330

    Implement Header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Icon} from '@iconify/react';
+import AddIcon from '@iconify/icons-ic/twotone-add';
+
+import styles from './Header.module.scss';
+import {Container} from "../container/Container";
+
+interface Props {
+}
+
+export const Header: React.FC<Props> = ({}) => {
+    return (
+        <header className={styles.Header}>
+            <Container className={styles.Container}>
+                <h1 className={styles.Title}>Task Management</h1>
+                <button className={styles.IconButton}>
+                    <Icon icon={AddIcon}/>
+                </button>
+            </Container>
+        </header>
+    );
+}
