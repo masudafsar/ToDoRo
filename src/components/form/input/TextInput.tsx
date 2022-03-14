39c2cabfb825bb4b7feaba0cd1f550b1b2f83fca
Masud Afsar <masud@afsar.info>
2022-03-15 00:13:51 +0330

    Fix ref issue of text input

--- src/components/form/input/TextInput.tsx
+++ src/components/form/input/TextInput.tsx
@@ -8,6 +8,7 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 export const TextInput = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
     return (
         <input
+            ref={ref}
             className={styles.TextInput}
             {...props}
             type="text"
