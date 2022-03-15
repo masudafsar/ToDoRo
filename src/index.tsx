641893deebd8456963975b3cb2466acc59be62ba
Masud Afsar <masud@afsar.info>
2022-03-15 10:25:50 +0330

    Use HashRouter instead of BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 import {TaskManagementApp} from './layouts/TaskManagementApp';
@@ -10,11 +10,11 @@ import './styles/index.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <TasksProvider>
                 <TaskManagementApp/>
             </TasksProvider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
